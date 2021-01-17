d6b5092456b54df16be39f75510a07b4b55068c8
karmasakshi <karmasakshi@users.noreply.github.com>
2021-01-18 00:00:04 +0400

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -1,1 +1,58 @@
-module.exports = {};
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+const headers = async () => [
+  {
+    headers: [
+      {
+        key: 'Access-Control-Allow-Origin',
+        value: 'https://uma-goyal.com'
+      },
+      {
+        key: 'Content-Security-Policy',
+        value: process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true' ? "connect-src 'self'; default-src 'none'; font-src https://fonts.gstatic.com; img-src 'self' data:; manifest-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; require-trusted-types-for 'script'; upgrade-insecure-requests;" : ''
+      },
+      {
+        key: 'Permissions-Policy',
+        value: 'autoplay=(), camera=(), fullscreen=(), geolocation=(), microphone=()'
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'no-referrer'
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'DENY'
+      },
+      {
+        key: 'X-XSS-protection',
+        value: '1; mode=block'
+      }
+    ],
+    source: '/(.*)'
+  }
+];
+
+const rewrites = async () => [
+  {
+    destination: 'https://cdn.splitbee.io/sb.js',
+    source: '/sb.js'
+  },
+  {
+    destination: 'https://hive.splitbee.io/:slug',
+    source: '/sb-api/:slug'
+  }
+];
+
+module.exports = {
+  headers,
+  reactStrictMode: true,
+  rewrites
+};
