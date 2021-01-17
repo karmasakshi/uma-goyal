8de46ab13b6ccd881c5d4638d46a7193164eb0a3
karmasakshi <karmasakshi@users.noreply.github.com>
2021-01-17 23:03:51 +0400

    build: add eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
+function HomePage(): JSX.Element {
+
+  return <div>Welcome to Next.js!</div>;
+
 }
 
-export default HomePage
+export default HomePage;
