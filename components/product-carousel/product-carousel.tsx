01b042e01f0bff911f4c5e333fa539dc00039a0c
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-02-07 13:51:00 +0400

    feat: update styles

--- components/product-carousel/product-carousel.tsx
+++ components/product-carousel/product-carousel.tsx
@@ -13,13 +13,13 @@ const ProductCarousel: (props: ProductCarouselProps) => JSX.Element = ({ product
 
       products.map((product: Product): JSX.Element => (
 
-        <div className="focus:outline-none focus:ring bg-white inline-block m-4 relative rounded" key={product.name} tabIndex={0}>
+        <div className="border border-gray-100 focus:outline-none focus:ring bg-white inline-block m-4 relative rounded" key={product.name} tabIndex={0}>
 
           <div className="overflow-hidden rounded shadow-md">
             <Image alt={product.name} height={256} src={product.imageUrl} width={256}></Image>
           </div>
 
-          <div className="absolute bg-white left-0 -mt-5 mx-4 right-0 rounded shadow-md">
+          <div className="absolute bg-white border border-gray-100 left-0 -mt-5 mx-4 right-0 rounded shadow-md">
             <span className="align-middle text-2xl">{product.name}</span>
           </div>
 
