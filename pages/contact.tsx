0f6be7d851a50350c5949d993fd98f5da8e2ba9a
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-03-27 10:28:50 +0400

    feat: update lint rules

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,20 +1,24 @@
-import Page from '@components/page/page';
-import Image from 'next/image';
-import { FunctionComponent, ReactElement } from 'react';
+import Page from "@components/page/page";
+import Image from "next/image";
+import { FunctionComponent, ReactElement } from "react";
 
 const Contact: FunctionComponent = (): ReactElement => (
-
   <Page title="Contact">
-
     <article>
-      <h1>If you&apos;d like to hire me for a freelance gig,<br />contact me on <a className="link" href="https://www.instagram.com/_umagoyal_/" rel="noreferrer" target="_blank">my Instagram</a>:</h1>
+      <h1>
+        <span>If you&apos;d like to hire me for a freelance gig,</span>
+        <br />
+        <span>contact me on </span>
+        <a className="link" href="https://www.instagram.com/_umagoyal_/" rel="noreferrer" target="_blank">
+          my Instagram
+        </a>
+        <span>:</span>
+      </h1>
       <a className="qr-code-container" href="https://www.instagram.com/_umagoyal_/" rel="noreferrer" target="_blank">
         <Image alt="Instagram QR Code" height={128} src="/qr-codes/instagram.png" width={128} />
       </a>
     </article>
-
   </Page>
-
 );
 
 export default Contact;
